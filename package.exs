4b16f48e98310e09d22a261833a3386ae4bff1cd
Michael Truog <mjtruog@gmail.com>
2014-10-02 12:04:37 -0700

    Add blank rebar.config.

--- package.exs
+++ package.exs
@@ -18,7 +18,7 @@ defmodule ErlangTerm.Mixfile do
   end
 
   defp package do
-    [files: ~w(src doc README.markdown LICENSE),
+    [files: ~w(src doc rebar.config README.markdown LICENSE),
      contributors: ["Michael Truog"],
      licenses: ["BSD"],
      links: %{"GitHub" => "https://github.com/okeuday/erlang_term"}]
