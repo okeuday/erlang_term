8612877bd9df1bba77a23aee51457173b252e220
Michael Truog <mjtruog@gmail.com>
2014-10-02 11:04:18 -0700

    Add hex.pm package.

---
+++ package.exs
@@ -0,0 +1,25 @@
+defmodule ErlangTerm.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :erlang_term,
+     version: "1.3.3",
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+  end
+
+  defp description do
+    "Provide the in-memory size of Erlang terms"
+  end
+
+  defp package do
+    [files: ~w(src doc README.markdown),
+     contributors: ["Michael Truog"],
+     licenses: ["BSD"],
+     links: %{"GitHub" => "https://github.com/okeuday/erlang_term"}]
+   end
+end
