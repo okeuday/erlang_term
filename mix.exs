4120993d8c9de8d4c8b398f5df2f14b769193b18
Michael Truog <mjtruog@gmail.com>
2017-01-10 16:28:16 -0800

    Version 1.6.0.

--- mix.exs
+++ mix.exs
@@ -3,11 +3,11 @@ defmodule ErlangTerm.Mixfile do
 
   def project do
     [app: :erlang_term,
-     version: "1.5.4",
+     version: "1.6.0",
      language: :erlang,
-     description: description,
-     package: package,
-     deps: deps]
+     description: description(),
+     package: package(),
+     deps: deps()]
   end
 
   defp deps do
